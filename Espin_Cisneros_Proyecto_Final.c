1bfefb69fa73a7e7347c06fdaa0304455dfecc20
Dominic <dominic.cisneros@udla.edu.ec>
2026-07-05 00:28:41 -0500

    busqueda, edición y eliminacion de libros

--- Espin_Cisneros_Proyecto_Final.c
+++ Espin_Cisneros_Proyecto_Final.c
@@ -83,15 +83,15 @@ void registrarLibro()
     getchar();
 
     printf("\nIngrese el titulo: ");
-    fgets(titulos[cantidadLibros], 50, stdin);
-    titulos[cantidadLibros][strcspn(titulos[cantidadLibros], "\n")] = '\0';
+    fgets(titulos[cantidadLibros],50,stdin);
+    titulos[cantidadLibros][strcspn(titulos[cantidadLibros],"\n")] = '\0';
 
     printf("Ingrese el autor: ");
-    fgets(autores[cantidadLibros], 50, stdin);
-    autores[cantidadLibros][strcspn(autores[cantidadLibros], "\n")] = '\0';
+    fgets(autores[cantidadLibros],50,stdin);
+    autores[cantidadLibros][strcspn(autores[cantidadLibros],"\n")] = '\0';
 
     printf("Ingrese el anio: ");
-    scanf("%d", &anios[cantidadLibros]);
+    scanf("%d",&anios[cantidadLibros]);
 
     cantidadLibros++;
 
@@ -108,26 +108,133 @@ void mostrarLibros()
 
     printf("\n========== LIBROS ==========\n");
 
-    for(int i = 0; i < cantidadLibros; i++)
+    for(int i=0;i<cantidadLibros;i++)
     {
-        printf("\nLibro %d\n", i + 1);
-        printf("Titulo: %s\n", titulos[i]);
-        printf("Autor : %s\n", autores[i]);
-        printf("Anio  : %d\n", anios[i]);
+        printf("\nLibro %d\n",i+1);
+        printf("Titulo: %s\n",titulos[i]);
+        printf("Autor : %s\n",autores[i]);
+        printf("Anio  : %d\n",anios[i]);
     }
 }
 
 void buscarLibro()
 {
-    printf("\nNo existen libros registrados para buscar.\n");
+    char tituloBuscar[50];
+    int encontrado = 0;
+
+    if(cantidadLibros == 0)
+    {
+        printf("\nNo existen libros registrados.\n");
+        return;
+    }
+
+    getchar();
+
+    printf("\nIngrese el titulo del libro: ");
+    fgets(tituloBuscar,50,stdin);
+    tituloBuscar[strcspn(tituloBuscar,"\n")] = '\0';
+
+    for(int i=0;i<cantidadLibros;i++)
+    {
+        if(strcmp(tituloBuscar,titulos[i]) == 0)
+        {
+            printf("\nLibro encontrado.\n");
+            printf("Titulo: %s\n",titulos[i]);
+            printf("Autor : %s\n",autores[i]);
+            printf("Anio  : %d\n",anios[i]);
+            encontrado = 1;
+        }
+    }
+
+    if(encontrado == 0)
+    {
+        printf("\nLibro no encontrado.\n");
+    }
 }
 
 void editarLibro()
 {
-    printf("\nNo existen libros registrados para editar.\n");
+    char tituloBuscar[50];
+    int encontrado = 0;
+
+    if(cantidadLibros == 0)
+    {
+        printf("\nNo existen libros registrados.\n");
+        return;
+    }
+
+    getchar();
+
+    printf("\nIngrese el titulo del libro que desea editar: ");
+    fgets(tituloBuscar,50,stdin);
+    tituloBuscar[strcspn(tituloBuscar,"\n")] = '\0';
+
+    for(int i=0;i<cantidadLibros;i++)
+    {
+        if(strcmp(tituloBuscar,titulos[i]) == 0)
+        {
+            printf("\nNuevo titulo: ");
+            fgets(titulos[i],50,stdin);
+            titulos[i][strcspn(titulos[i],"\n")] = '\0';
+
+            printf("Nuevo autor: ");
+            fgets(autores[i],50,stdin);
+            autores[i][strcspn(autores[i],"\n")] = '\0';
+
+            printf("Nuevo anio: ");
+            scanf("%d",&anios[i]);
+
+            printf("\nLibro editado correctamente.\n");
+            encontrado = 1;
+            break;
+        }
+    }
+
+    if(encontrado == 0)
+    {
+        printf("\nLibro no encontrado.\n");
+    }
 }
 
 void eliminarLibro()
 {
-    printf("\nNo existen libros registrados para eliminar.\n");
+    char tituloBuscar[50];
+    int encontrado = 0;
+
+    if(cantidadLibros == 0)
+    {
+        printf("\nNo existen libros registrados.\n");
+        return;
+    }
+
+    getchar();
+
+    printf("\nIngrese el titulo del libro que desea eliminar: ");
+    fgets(tituloBuscar,50,stdin);
+    tituloBuscar[strcspn(tituloBuscar,"\n")] = '\0';
+
+    for(int i=0;i<cantidadLibros;i++)
+    {
+        if(strcmp(tituloBuscar,titulos[i]) == 0)
+        {
+            for(int j=i;j<cantidadLibros-1;j++)
+            {
+                strcpy(titulos[j],titulos[j+1]);
+                strcpy(autores[j],autores[j+1]);
+                anios[j]=anios[j+1];
+            }
+
+            cantidadLibros--;
+
+            printf("\nLibro eliminado correctamente.\n");
+
+            encontrado = 1;
+            break;
+        }
+    }
+
+    if(encontrado == 0)
+    {
+        printf("\nLibro no encontrado.\n");
+    }
 }
